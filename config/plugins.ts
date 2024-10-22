40fd7a2b4a15a32aa132bd618347ad059a74455d
natalia <nespinozabar@gmail.com>
2024-10-22 18:28:08 -0500

    .evn

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,17 +1,16 @@
-// plugins.ts
 module.exports = {
     // Otras configuraciones de plugins...
     
     'strapi-neon-tech-db-branches': {
       enabled: true, // Habilita el plugin
       config: {
-        neonApiKey: "1yxyjbpyhsiejzd1wx7006569tz43w2y20hchyfe0dd93csadm5survmurdmwn61", // Tu API key de Neon
-        neonProjectName: "db-strapi-post", // Nombre del proyecto en Neon donde corre tu base de datos
-        neonRole: "bdpost_owner", // Rol creado manualmente en Neon bajo Roles
-        gitBranch: "main" // Rama de git que puedes fijar con esta opción de configuración
+        neonApiKey: process.env.NEON_API_KEY, // Tu API key de Neon
+        neonProjectName: process.env.NEON_PROJECT_NAME, // Nombre del proyecto en Neon donde corre tu base de datos
+        neonRole: process.env.NEON_ROLE, // Rol creado manualmente en Neon bajo Roles
+        gitBranch: process.env.GIT_BRANCH // Rama de git que puedes fijar con esta opción de configuración
       }
     },
-  
+    
     // Otras configuraciones de plugins...
   };
   
